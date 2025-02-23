c6b4919a441a2dd3b7dbceec41ff0d8d11b6e3fc
Nguyen Duy Hung <127693983+kharina21@users.noreply.github.com>
2025-02-23 17:14:13 +0700

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,77 @@
 import { useState } from "react";
-import { Button, IconButton } from "@mui/material";
-import { Delete, Home as HomeIcon } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { Delete, ForkRight, Home as HomeIcon } from "@mui/icons-material";
+import { useColorScheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+
+//icon
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+//Box
+import Box from "@mui/material/Box";
+
+function SelectSmall() {
+    const { mode, setMode } = useColorScheme();
+    const handleChange = (event) => {
+        setMode(event.target.value);
+    };
+
+    return (
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <Select
+                labelId="label-select-dark-light-mode"
+                id="select-dark-light-mode"
+                value={mode}
+                onChange={handleChange}
+            >
+                <MenuItem value="light">
+                    <Box display="flex" alignItems="center" gap={1}>
+                        <LightModeIcon fontSize="small" />
+                        Light
+                    </Box>
+                </MenuItem>
+                <MenuItem value="dark">
+                    <Box display="flex" alignItems="center" gap={1}>
+                        <DarkModeOutlinedIcon fontSize="small" />
+                        Dark
+                    </Box>
+                </MenuItem>
+                <MenuItem value="system">
+                    <Box display="flex" alignItems="center" gap={1}>
+                        <SettingsBrightnessIcon fontSize="small" />
+                        System
+                    </Box>
+                </MenuItem>
+            </Select>
+        </FormControl>
+    );
+}
+
+function ModeToggle() {
+    const { mode, setMode } = useColorScheme();
+    return (
+        <Button
+            onClick={() => {
+                setMode(mode === "light" ? "dark" : "light");
+            }}
+        >
+            {mode === "light" ? "Turn dark" : "Turn light"}
+        </Button>
+    );
+}
+
 function App() {
     const [count, setCount] = useState(0);
     return (
         <>
             <h1>kha21dev</h1>
-            <div>
-                <Button variant="text">Hello button</Button>
-                <Button variant="contained">Hello button</Button>
-                <Button variant="outlined">Hello button</Button>
-            </div>
-            <div>
-                <HomeIcon color="success" />
-            </div>
-            <div>
-                <Button variant="contained" sx={{ color: red[500] }}>
-                    <Delete />
-                </Button>
-            </div>
+            <SelectSmall />
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import theme from "./theme.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
+        <CssVarsProvider theme={theme}>
             <CssBaseline />
             <App />
-        </ThemeProvider>
+        </CssVarsProvider>
     </StrictMode>
 );
